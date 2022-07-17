9e50a9282013cc8146e780a1f902623a7891b2d4
Jude Su <fortunatemaker2603@gmail.com>
2022-07-17 13:45:53 +0800

    stage 1 finished

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -1,7 +1,142 @@
 package tictactoe;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the cells: ");
+        String initSetup = scanner.next();
+        char[][] matrix = new char[3][3];
+        int countX = 0;
+        int countO = 0;
+
+        for (int i = 0; i < initSetup.length(); i++) {
+            int rowIndex = i / 3;
+            int columnIndex = i % 3;
+
+            if (!Objects.equals('_',initSetup.charAt(i))) {
+                matrix[rowIndex][columnIndex] = initSetup.charAt(i);
+                if (matrix[rowIndex][columnIndex] == 'O') {
+                    countO += 1;
+                } else {
+                    countX += 1;
+                }
+            } else {
+                matrix[rowIndex][columnIndex] = ' ';
+            }
+        }
+
+        printStatus(matrix);
+
+        int rowPicked, columnPicked;
+
+        while (true) {
+            System.out.println("Enter the coordinates: ");
+            if (scanner.hasNextInt()) {
+                rowPicked = scanner.nextInt();
+            } else {
+                System.out.println("You should enter numbers!");
+                scanner.next();
+                continue;
+            }
+
+            if (scanner.hasNextInt()) {
+                columnPicked = scanner.nextInt();
+            } else {
+                System.out.println("You should enter numbers!");
+                scanner.next();
+                continue;
+            }
+
+            if (rowPicked <= 0 || rowPicked > 3 || columnPicked <= 0 || columnPicked > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            if (matrix[rowPicked-1][columnPicked-1] != ' ') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+            break;
+        }
+
+        matrix[rowPicked-1][columnPicked-1] = countO == countX ? 'X' : 'O';
+        printStatus(matrix);
+        printResult(matrix);
+
+    }
+
+    private static void printResult(char[][] matrix) {
+        char[] rowWin = new char[] {'U', 'U', 'U'};
+        char[] colWin = new char[] {'U', 'U', 'U'};
+        char[] diagWin = new char[] {'U', 'U'};
+        boolean gameFinished = true;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == ' ') {
+                    gameFinished = false;
+                }
+                if (rowWin[i] == 'U') {
+                    rowWin[i] = matrix[i][j];
+                } else if (rowWin[i] != matrix[i][j]) {
+                    rowWin[i] = 'N';
+                } else if (j==2 && matrix[i][j] != ' ') {
+                    System.out.printf("%s wins\n", matrix[i][j]);
+                    return;
+                }
+
+                if (colWin[j] == 'U') {
+                    colWin[j] = matrix[i][j];
+                } else if (colWin[j] != matrix[i][j]) {
+                    colWin[j] = 'N';
+                } else if (i==2 && matrix[i][j] != ' ') {
+                    System.out.printf("%s wins\n", matrix[i][j]);
+                    return;
+                }
+
+                if (i == j) {
+                    if (diagWin[0] == 'U') {
+                        diagWin[0] = matrix[i][j];
+                    } else if (diagWin[0] != matrix[i][j]) {
+                        diagWin[0] = 'N';
+                    } else if (i==2 && matrix[i][j] != ' ') {
+                        System.out.printf("%s wins\n", matrix[i][j]);
+                        return;
+                    }
+                }
+
+                if (i == (2-j)) {
+                    if (diagWin[1] == 'U') {
+                        diagWin[1] = matrix[i][j];
+                    } else if (diagWin[1] != matrix[i][j]) {
+                        diagWin[1] = 'N';
+                    } else if (i==2 && matrix[i][j] != ' ') {
+                        System.out.printf("%s wins\n", matrix[i][j]);
+                        return;
+                    }
+                }
+            }
+        }
+        if (gameFinished){
+            System.out.print("Draw\n");
+        } else {
+            System.out.print("Game not finished\n");
+        }
+
+    }
+
+    private static void printStatus(char[][] matrix) {
+        System.out.println("---------");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (j == 0) { System.out.print("| "); }
+                System.out.printf("%s ", matrix[i][j]);
+                if (j==2) { System.out.print("|\n"); }
+            }
+        }
+        System.out.println("---------");
     }
 }
